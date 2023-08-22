b67859690b1e5d7ee7f1858f0128a13736daebcb
gregmkorte <gregmkorte@gmail.com>
2023-08-22 14:45:20 -0500

    Adding display products and remove product functionality

--- Program.cs
+++ Program.cs
@@ -119,3 +119,111 @@ List<Product> products = new List<Product>()
         ProductTypeId = 2
     }
 };
+
+Console.Clear();
+string greeting = @"Welcome to Melody Mart
+Get your shit and get out";
+
+Console.WriteLine(greeting);
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"Choose an option:
+0. Exit
+1. View all products
+2. View products by type
+3. Purchase a product
+4. Add a new product
+5. Remove from inventory
+6. View product details
+7. Update product info
+8. View inventory statistics");
+    choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "0":
+            Console.Clear();
+            Console.WriteLine("Seeya shithead");
+            break;
+        case "1":
+            Console.Clear();
+            ViewAllProducts();
+            TheCleanerUpper();
+            break;
+        case "2":
+            throw new NotImplementedException("Feature not available, dumbass");
+        case "3":
+            throw new NotImplementedException("Feature not available, dumbass");
+        case "4":
+            throw new NotImplementedException("Feature not available, dumbass");
+        case "5":
+            Console.Clear();
+            RemoveProduct();
+            break;
+        case "6":
+            throw new NotImplementedException("Feature not available, dumbass");
+        case "7":
+            throw new NotImplementedException("Feature not available, dumbass");
+        case "8":
+            throw new NotImplementedException("Feature not available, dumbass");
+
+        default:
+            Console.Clear();
+            Console.WriteLine("Pick an actual option, shithead. Press any key to try again, shithead.");
+            Console.WriteLine("END OF LINE");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+    }
+}
+
+void TheCleanerUpper()
+{
+    Console.WriteLine("Press any key to return to menu");
+    Console.ReadKey();
+    Console.Clear();
+}
+
+
+void ViewAllProducts()
+{
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name} {(products[i].DateSold == null ? "is available" : "was sold")} for ${products[i].Price}");
+    }
+}
+void RemoveProduct()
+{
+    Console.WriteLine("Please choose a product to remove:");
+    ViewAllProducts();
+    //need to put in a while and refactor
+    try
+    {
+        int response = int.Parse(Console.ReadLine().Trim());
+        products.RemoveAt(response - 1);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please type only integers!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        Console.WriteLine("Do Better!");
+    }
+}
+/*
+
+1. Show by type
+1. Be able to purchase instrument
+1. Post new instrument
+1. Remove from inventory
+1. See how long an item has been in inventory
+    1. See details
+1. Update prices
+1. Stats
+    1. Total purchases
+    1. Total sold $ amount
+    1. How many of each type instrument in stock
+1. Pick a random stock item for details */
